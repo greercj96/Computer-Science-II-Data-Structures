84e6a0b51dd68e9abefc5d981c020615763e1192
greercj96 <43151563+greercj96@users.noreply.github.com>
2019-10-11 23:32:00 +0200

    Add files via upload

---
+++ cppInheritance/Inheritance.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+
+class Vehicle{
+private:
+	double weight;
+	int numTires;
+	int numPassengers;
+	string model;
+
+public:
+	void setModel(string model) {
+		model = model;
+		cout << "This is a "<< model << endl;
+	}
+
+
+	void setnumTires(int numTires) {
+		cin >> numTires;
+		cout << "There are " << numTires << " tires" << endl;
+	}
+
+	void setnumPassengers(int numPassengers) {
+		numPassengers = numPassengers;
+		cout << "You have " << numPassengers << " passengers" << endl;
+	}
+};
+
+class Car : public Vehicle {
+
+private:
+	bool radio = true;
+	int numDoors;
+
+public:
+	Car() {
+		numDoors = 4;
+		cout << "This is a car ";
+		cout << "This vehicle has a radio " << endl;
+	}
+
+
+};
+
+
+class Bike : public Vehicle {
+private:
+	string helmetSize;
+
+public:
+	Bike() {
+
+	}
+
+};
+
---
+++ cppInheritance/InheritanceProgram.cpp
@@ -0,0 +1,11 @@
+#include "Inheritance.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	Car car1;
+	car1.setModel("Mercedes");
+	car1.setnumPassengers(2);
+
+}
