abef9c7f4e03a177ac114b2b8c42a7dfbe2a7667
greercj96 <43151563+greercj96@users.noreply.github.com>
2019-10-12 21:24:51 +0200

    Update InheritanceProgram.cpp

--- cppInheritance/InheritanceProgram.cpp
+++ cppInheritance/InheritanceProgram.cpp
@@ -6,6 +6,13 @@ using namespace std;
 int main() {
 	Car car1;
 	car1.setModel("Mercedes");
-	car1.setnumPassengers(2);
+	car1.setnumPassengers(4);
 
+	cout << "*** NEXT VEHICLE ***" << endl;
+
+	Bike bike1;
+	bike1.setModel("Harley Davidson");
+	bike1.setnumPassengers(2);
+	bike1.setHelmetSize("Small");
 }
+ 
