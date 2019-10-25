d4d1ca377e14d538be4e8a59acf681d100edc328
greercj96 <43151563+greercj96@users.noreply.github.com>
2019-10-25 12:19:55 +0200

    Add files via upload

---
+++ cppLinkedList/cppLinkedListsStack.cpp
@@ -0,0 +1,121 @@
+#include <iostream>
+
+using namespace std; 
+
+class node {
+public: 
+	int data;
+	node *next;
+	
+	
+};
+
+
+class linkedList {
+	private:
+		node* head, *tail;
+		node* top= NULL;
+	public:
+		linkedList() {
+			head= NULL;
+			tail= NULL;
+			
+		}
+
+
+	bool isempty() {
+		if (top == NULL) {
+			return true;
+		}
+		else {
+			return false;
+		}
+
+	}
+
+	void push(int data) {
+		node* newNode = new node();
+		newNode->data = data;
+		newNode->next = top;
+		top = newNode;
+	}
+
+	void pop() {
+		if (isempty()) {
+			cout << "Stack is Empty";
+		}
+		else {
+			node* temp = top;
+			top = top->next;
+			delete(temp);
+		}
+	}
+		void insertNode(int n) {
+			node* newNode = new node;
+			newNode->data = n;
+			newNode->next = NULL;
+			if (head == NULL) {
+				head = newNode;
+				tail = newNode;
+			}
+			else {
+				tail->next = newNode;
+				tail = tail->next;
+			}
+
+		}
+
+		void getElementByIndex(int index) {
+			node* current = head;
+			int searchedElement;
+			int count = 0;
+			while (current != NULL) {
+				if (count == index)
+					cout << "\nThe data element in node " << index << " is " << current->data << endl;
+				count++;
+				current = current->next;
+			}
+		}
+		void printList() {
+			node* temp = new node;
+			temp = head;
+			while (temp != NULL) {
+				cout << temp->data;
+				temp = temp->next;
+			}
+			cout << "End of list";
+
+		}
+
+		void printStack() {
+			node* temp = new node;
+			temp = top;
+			while (temp != NULL) {
+				cout << "\n";
+				cout << temp->data<< "\n";
+				cout << "|\n";
+				temp = temp->next;
+			}
+			cout << "End of list";
+		}
+	};
+
+int main() {
+	linkedList a;
+	/*a.insertNode(1);
+	a.insertNode(2);
+	a.insertNode(3);
+	a.insertNode(4);
+	a.printList();
+	a.getElementByIndex(1);*/
+	a.push(1);
+	a.push(2);
+	a.push(3);
+	a.pop();
+	a.printStack();
+	a.pop();
+	a.push(3);
+	a.printStack();
+	
+	return 0;
+}
