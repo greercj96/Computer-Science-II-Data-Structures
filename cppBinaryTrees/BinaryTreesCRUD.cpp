b2c9997336a38fe45514dfe85a5e7499c6efb74f
greercj96 <43151563+greercj96@users.noreply.github.com>
2019-10-26 18:56:49 +0200

    Update BinaryTreesCRUD.cpp

--- cppBinaryTrees/BinaryTreesCRUD.cpp
+++ cppBinaryTrees/BinaryTreesCRUD.cpp
@@ -88,23 +88,81 @@ BTree* updateNode(BTree* rootNode, int oldData, int newData) {
 	return rootNode;
 }
 
+struct Trunk
+{
+    Trunk *prev;
+    string str;
 
+    Trunk(Trunk *prev, string str)
+    {
+        this->prev = prev;
+        this->str = str;
+    }
+};
 
+// Helper function to print branches of the binary tree
+void showTrunks(Trunk *p)
+{
+    if (p == nullptr)
+        return;
+
+    showTrunks(p->prev);
+
+    cout << p->str;
+}
+
+// Recursive function to print binary tree
+// It uses inorder traversal
+void printTree(BTree *rootNode, Trunk *prev, bool isLeft)
+{
+    if (rootNode == nullptr)
+        return;
+    
+    string prev_str = "    ";
+    Trunk *trunk = new Trunk(prev, prev_str);
+
+    printTree(rootNode->rightNode, trunk, true);
+
+    if (!prev)
+        trunk->str = "---";
+    else if (isLeft)
+    {
+        trunk->str = ".---";
+        prev_str = "   |";
+    }
+    else
+    {
+        trunk->str = "`---";
+        prev->str = prev_str;
+    }
+
+    showTrunks(trunk);
+    cout << rootNode->data << endl;
+
+    if (prev)
+        prev->str = prev_str;
+    trunk->str = "   |";
+
+    printTree(rootNode->leftNode, trunk, false);
+}
 
 
 int main()
 {
+    
 
 	BTree root(8);
 	BTree* rootNode = &root;
-	cout << rootNode->data << endl;
-	insert(5, rootNode);
-	insert(10, rootNode);
-	insert(7, rootNode);
-	insert(2, rootNode);
-	insert(2, rootNode);
-	insert(9, rootNode);
-	insert(11, rootNode);
+	rootNode->leftNode = new BTree(5);
+	rootNode->rightNode = new BTree(10);
+	rootNode->leftNode->rightNode = new BTree(7);
+	rootNode->leftNode->leftNode = new BTree(2);
+	rootNode->rightNode->leftNode = new BTree(9);
 
+	
+	printTree(rootNode, nullptr, false);
+	return 0;
+	
+	
 
 }
